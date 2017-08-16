7719518a46042adeafd1df97f602b2148d50ae14
cstieg <cstieg4899@yahoo.com>
2017-08-15 20:22:10 -0400

    Add: ShoppingCart controller from previous incarnation

---
+++ Deerfly_Patches/Controllers/ShoppingCartController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Deerfly_Patches.ViewModels;
+using Deerfly_Patches.Modules;
+
+namespace Deerfly_Patches.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            ShoppingCart shoppingCart = HttpContext.Session.GetObjectFromJson<ShoppingCart>("_shopping_cart");
+            return View(shoppingCart);
+        }
+    }
+}
