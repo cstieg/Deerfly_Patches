8b68c782439c26c11a055a25f8110f64c1338dc3
cstieg <cstieg4899@yahoo.com>
2017-08-23 11:35:46 -0400

    Refactor: Encapsulate retailer maps functions in object

--- Deerfly_Patches/Scripts/Deerfly_Patches/gmaps.js
+++ Deerfly_Patches/Scripts/Deerfly_Patches/gmaps.js
@@ -1,35 +1,45 @@
-﻿
+﻿var gmaps = {
+    init: function () {
+        this.retailerMap;
+        this.defaultMapLocation = new LatLng(43, -86);
+        this.displayMap();
+        this.setMapToCurrentLocation();
+    },
+
+    displayMap: function (location = this.defaultMapLocation) {
+        var mapElement = $('#retailer-map-view')[0];
+        this.retailerMap = new google.maps.Map(mapElement, {
+            zoom: 15,
+            center: location
+        });
+    },
+
+    setMapToCurrentLocation: function (map = this.retailerMap) {
+        $.getJSON('http://freegeoip.net/json/', function (data) {
+            debugger;
+            map.setCenter(new google.maps.LatLng(data.latitude, data.longitude));
+        });
+    },
+
+    getCurrentLocation: function() {
+        var data = $.ajax({
+            url: 'http://freegeoip.net/json/',
+            dataType: 'json',
+            async: false,
+            error: function (err) {
+                debugger;
+                alert("Couldn't determine current location!");
+            }
+        });
+
+        var response = data.responseJSON;
+        return new LatLng(response.latitude, response.longitude);
+    }
+};
 
 function initialMap() {
-    displayMap();
+    gmaps.init();
 }
-
-function displayMap() {
-    var location = getCurrentLocation();
-    if (!location) { return; }
-    var mapElement = $('#retailer-map-view')[0];
-    var map = new google.maps.Map(mapElement, {
-        zoom: 15,
-        center: location
-    });
-}
-
-function getCurrentLocation() {
-    
-    var data = $.ajax({
-        url: 'http://freegeoip.net/json/',
-        dataType: 'json',
-        async: false,
-        error: function (err) {
-            debugger;
-            alert("Couldn't determine current location!");
-        }
-    });
-
-    var response = data.responseJSON;
-    return new LatLng(response.latitude, response.longitude);
-}
-
 
 class LatLng {
     constructor(lat, lng) {
