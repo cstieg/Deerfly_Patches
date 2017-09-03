3a7553e1609a14911fcf3215f765efec5eac027f
cstieg <cstieg4899@yahoo.com>
2017-09-03 14:34:13 -0400

    Add: Upload image preview (products)

--- Deerfly_Patches/Scripts/Deerfly_Patches/site.js
+++ Deerfly_Patches/Scripts/Deerfly_Patches/site.js
@@ -211,3 +211,18 @@ function displayTestimonial(id, url) {
     $('.testimonial-lable').removeClass('selected-testimonial');
     $('#' + id).addClass('selected-testimonial');
 }
+
+
+/* ****************************** Image Upload Preview ***************************************** */
+function imageUploadPreview(targetId) {
+    var e = window.event;
+    for (var i = 0; i < e.srcElement.files.length; i++) {
+        var file = e.srcElement.files[i];
+        var $targetImg = $(targetId);
+        var reader = new FileReader();
+        reader.onloadend = function () {
+            $targetImg.attr("src", reader.result);
+        }
+        reader.readAsDataURL(file);
+    }
+}
