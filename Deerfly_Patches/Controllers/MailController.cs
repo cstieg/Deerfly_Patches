264b98888c0e8f2420b422970f16f9422bd2cc95
cstieg <cstieg4899@yahoo.com>
2018-02-13 16:08:25 -0500

    Fix: Get base url from server for mail controller

--- Deerfly_Patches/Controllers/MailController.cs
+++ Deerfly_Patches/Controllers/MailController.cs
@@ -6,6 +6,7 @@ using RazorEngine.Templating;
 using System.Data.Entity;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
 using System.Web.Mvc;
@@ -14,6 +15,12 @@ namespace DeerflyPatches.Controllers
 {
     public class MailController : BaseController
     {
+        public async Task Test()
+        {
+            string cart = "PAY-8279066855591091LLJ7BRAA";
+            await ConfirmOrder(cart);
+        }
+
         // POST: Mail/ConfirmOrder?cart=DF39FEI314040
         /// <summary>
         /// Sends confirmation email for completed order.
@@ -21,34 +28,32 @@ namespace DeerflyPatches.Controllers
         /// <param name="cart">Alphanumeric cart id assigned to order by PayPal</param>
         /// <returns>JSON success=true result </returns>
         [HttpPost]
-        public async Task<ActionResult> ConfirmOrder()
+        public async Task<ActionResult> ConfirmOrder(string cart)
         {
             ApplicationDbContext db = new ApplicationDbContext();
 
             // find order
-            string id = Request.Params.Get("cart");
-            Order order = await db.Orders.Include(o => o.Customer).Where(o => o.Cart == id).SingleOrDefaultAsync();
+            Order order = await db.Orders.Include(o => o.Customer).Where(o => o.Cart == cart).SingleOrDefaultAsync();
             if (order == null)
             {
                 return HttpNotFound();
             }
+            var a = order.OrderDetails.First().Product.WebImages.First().ImageUrl;
             order.ShipToAddress = await db.Addresses.FirstOrDefaultAsync(o => o.Id == order.ShipToAddressId);
-
+            
             // Add baseURL for images to viewBag
             var viewBag = new DynamicViewBag();
-            string baseUrl = Server.MapPath("~");
-            baseUrl = baseUrl.Substring(0, baseUrl.Length - 1);
+            string baseUrl = ControllerHelper.GetBaseUrl(Request);
             viewBag.AddValue("baseURL", baseUrl);
 
             // render email body
             string templatePath = Server.MapPath("~/Views/Mail/OrderSuccessEmail.cshtml");
             var sr = new StreamReader(templatePath);
-            string body = Engine.Razor.RunCompile(await sr.ReadToEndAsync(), "orderSuccessEmail", null, order, viewBag);
+            string body = Engine.Razor.RunCompile(await sr.ReadToEndAsync(), "OrderSuccessEmail", null, order, viewBag);
 
             // create email message
             var message = new MailMessage
             {
-                From = new MailAddress("webmaster@stovepipeheatsaver.com"),
                 Subject = "Order confirmation - " + order.Description,
                 Body = body,
                 IsBodyHtml = true
@@ -58,6 +63,10 @@ namespace DeerflyPatches.Controllers
             // send email
             using (var smtp = new SmtpClient())
             {
+                // get sender account from web.config - system.net
+                string from = ((NetworkCredential)smtp.Credentials).UserName;
+                message.From = new MailAddress(from);
+               
                 await smtp.SendMailAsync(message);
             }
             return this.JOk();
